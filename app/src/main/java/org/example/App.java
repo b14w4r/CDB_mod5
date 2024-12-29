8719d65703a9d43788c8bd9ef548af9edd81be65
b14w4r <spiritedbeaver@yandex.ru>
2024-12-29 23:26:26 +0300

    clean up

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
   public String getGreeting() {
-    return "Hello WWorld!";
+    return "Hello World!";
   }
 
   public static void main(String[] args) {
